3b9bd0a92f91ae579aee7a285bf72be8c5e1be8d
Edward Koulakidis <Edouardos@csd.auth.gr>
2022-04-07 12:46:16 +0300

    Removed pointer_,added templates,moved struct outside of class,changed NULL to nullptr

--- src/binary_search_tree.h
+++ src/binary_search_tree.h
@@ -2,42 +2,43 @@
 #define BINARY_SEARCH_TREE_H
 
 #include <iostream>
-
+template <typename T>
+struct Node {
+  T data;
+  Node* left;
+  Node* right;
+  Node() {
+    left = right = nullptr;
+    data = 0;
+  }
+  Node(const T& e) {
+    data = e;
+    left = right = nullptr;
+  }
+};
+template <typename T>
 class BinarySearchTree {
-protected:
-  struct Node {//this struct will be used to store the tree's leaves
-    std::string first_word;
-    std::string second_word;
-    Node *left;
-    Node *right;
-    unsigned int counter;//variable to count how many times the tuple of words has been found
-
-  //every time a new node is created this function is called to initialize the node's data
-    Node(const std::string &first_word, const std::string &second_word) {
-      this->first_word = first_word;
-      this->second_word = second_word;
-      right = NULL;
-      left = NULL;
-      counter = 1;
-    }
-  };
-  Node *root_; //pointer at the root of the tree
-  Node *pointer_;// assistant pointer to cross the tree's nodes
-public:
-  BinarySearchTree() {//tree's constructor used to create an empty tree
-    root_ = NULL;
-    pointer_ = NULL;
+ protected:
+  Node<T>* root_;  // pointer at the root of the tree
+ public:
+  BinarySearchTree() {  // tree's constructor used to create an empty tree
+    root_ = nullptr;
   }
 
-  void DeleteNode(Node *pointer);//assistant function used by the destructor
+  void DeleteNode(
+      Node<T>* pointer);  // assistant function used by the destructor
 
-  ~BinarySearchTree(){//the destructor used to delete the tree
+  ~BinarySearchTree() {  // the destructor used to delete the tree
     DeleteNode(root_);
   }
 
-  void Insert(const std::string& word1, const std::string& word2);//function to insert a new tuple of words
-  
-  bool Search(const std::string& word1, const std::string& word2);//function to search for a specific tuple of words
+  void Insert(
+      const std::string& word1,
+      const std::string& word2);  // function to insert a new tuple of words
+
+  bool Search(const std::string& word1,
+              const std::string&
+                  word2);  // function to search for a specific tuple of words
 };
 
 #endif
